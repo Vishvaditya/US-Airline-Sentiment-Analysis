337214cc9791ce7bfa83555e6a3cb577aa473b88
Vishvaditya <vishvadityaluhach@gmail.com>
2021-05-22 17:10:45 +0530

    Created radio buttons to display Tweets

--- app.py
+++ app.py
@@ -9,5 +9,24 @@ st.markdown(
     "This Dashboard visualises sentiment anlaysis of tweets regarding US Airlines"
 )
 
-tweets = pd.read_csv("Tweets.csv")  # loaded Tweets.csv file in pandas dataframe
+
+@st.cache(persist=True)  # Function decorator to cache data
+def load_data():
+    tweets = pd.read_csv("Tweets.csv")  # loaded Tweets.csv file in pandas dataframe
+    tweets["tweet_created"] = pd.to_datetime(tweets["tweet_created"])
+    return tweets
+
+
+tweets = load_data()
+
+st.sidebar.subheader("Display Tweet")
+random_tweet = st.sidebar.radio("Sentiment", ("Positive", "Negative", "Neutral"))
+st.sidebar.markdown(
+    tweets.query("airline_sentiment == @random_tweet.lower()")[["text"]]
+    .sample(n=1)
+    .iat[0, 0]
+)
+
+
+# st.write(tweets)
 
