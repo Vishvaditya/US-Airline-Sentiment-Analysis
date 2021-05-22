ae983c8d39119ddf008daaf0f2ab7b0d6328e18b
Vishvaditya <vishvadityaluhach@gmail.com>
2021-05-22 16:24:21 +0530

    Loaded tweets data into dataframe

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+st.title("US Airline Sentiment Analysis")
+st.sidebar.title("Sentiment Analysis of Tweets using Streamlit")
+
+st.markdown(
+    "This Dashboard visualises sentiment anlaysis of tweets regarding US Airlines"
+)
+
+tweets = pd.read_csv("Tweets.csv")  # loaded Tweets.csv file in pandas dataframe
+
